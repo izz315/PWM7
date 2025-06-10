da513c2294480f99dd37bbbea0b206b024ef4b02
izz315 <154888989+izz315@users.noreply.github.com>
2025-06-10 20:59:26 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", async event => {
   const cache = await caches.open("pwa-assets");
   // it stores all resources on first SW install
-  cache.addAll(["/", "app.js", "icons8-instagram-50.png", "icons8-linkedin-50.png", "icons8-whatsapp-50.png", "icons8-x-50.png", "izz.jpg"]); 
+  cache.addAll(["/", "app.js", "manifest.json", "index.html", "icons8-instagram-50.png", "icons8-linkedin-50.png", "icons8-whatsapp-50.png", "icons8-x-50.png", "izz.jpg"]); 
 });
  
 self.addEventListener("fetch", event => {
